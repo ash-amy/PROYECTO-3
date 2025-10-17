e2a34da4c6c5fa97e27a93e38462e880eaf043aa
cam <cchumbemalarin@gmail.com>
2025-10-17 11:20:37 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Inicio from "./components/Inicio";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="Sobre Nosotros" element={<h1>Sobre Nosotros</h1>} />
+          <Route path="Juegos y Retos" element={<h1>Juegos y Retos</h1>} />
+          <Route path="Contacto" element={<h1>Contacto</h1>} />
+          <Route path="Testimonios" element={<h1>Testimonios</h1>} />
+          <Route path="Formulario" element={<h1>Formulario</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <nav className="navbar degradado p-5 text-black ">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-xl font-bold">
+          <img
+            className="logo"
+            src="./Public/logo-largo.png"
+            alt="Logo Code Play"
+          />
+        </div>
+
+        <ul className="options flex space-x-6">
+          <li className="hover:text-gray-300">
+            {" "}
+            <Link to="/">Inicio</Link>
+          </li>
+          <li className="hover:text-gray-300">
+            {" "}
+            <Link to="Sobre Nosotros">Sobre Nosotros</Link>
+          </li>
+          <li className="hover:text-gray-300">
+            {" "}
+            <Link to="Juegos y Retos">Juegos y Retos</Link>
+          </li>
+          <li className="hover:text-gray-300">
+            {" "}
+            <Link to="Contacto">Contacto</Link>{" "}
+          </li>
+          <li className="hover:text-gray-300">
+            {" "}
+            <Link to="Testimonios">Testimonios</Link>
+          </li>
+        </ul>
+
+        <button className="boton hover:text-gray-300">
+          {""}
+          <Link to="Formulario"></Link>Matricúlate
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
